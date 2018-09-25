4cd440e6314415a5b4a71f26375faffcae742180
enolamsean <33710544+enolamsean@users.noreply.github.com>
2018-09-24 23:13:18 -0400

    Placement of contents with filler content

--- oddFutureFunc.js
+++ oddFutureFunc.js
@@ -2,11 +2,14 @@
 function menuToggle(){
 	var y = document.getElementById("menu")
 	var x = document.getElementById("nav");
-	if(x.style.top === "-96.25%"){
+	if(x.style.top === "-93.25%"){
 		x.style.top = "0%";
 		y.style.opacity = "0";
 	} else{
-		x.style.top = "-96.25%";
+		x.style.top = "-93.25%";
 		y.style.opacity = "1";
 	}
 }
+
+//document.getElementById('rightHere').focus();
+
